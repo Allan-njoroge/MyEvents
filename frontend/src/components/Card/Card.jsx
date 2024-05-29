eeef6f3999164f8941cc3cfd8d9e7bed1c7d69e2
Allan-njoroge <allandavis254@gmail.com>
2024-05-29 22:58:48 +0300

    Edited the card data to be dynamic

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -5,20 +5,20 @@ import { FaCalendarAlt, FaClock, FaTicketAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
 
-const Card = ({ image, title, date, startTime, endTime, location, price }) => {
+const Card = ({ image, title, date, startTime, endTime, location, price, id }) => {
   return (
     <div className='event-card'>
         <img src={Image} alt="" className='event-img' />
         <div className='event-details'>
             <h3 className="event-title">
-                <Link>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem reiciendis, quaerat magnam accusantium dolorum voluptatum est tempore quo eveniet!</Link>
+                <Link to={`/${id}`}>{title}</Link>
             </h3>
             {/*--event date, time, location and price--*/}
             <div className='details-wrapper'>
-                <p className='details'> <FaCalendarAlt className='event-icon' /> December 20th 2024</p>
-                <p className='details'> <FaClock className="event-icon" /> 8:00AM - 5:00PM</p>
-                <p className='details'> <FaLocationDot className='event-icon' /> Nairobi, CBD</p>
-                <p className='details'> <FaTicketAlt className='event-icon' /> Ksh 1000</p>
+                <p className='details'> <FaCalendarAlt className='event-icon' /> {date}</p>
+                <p className='details'> <FaClock className="event-icon" /> {startTime} - {endTime}</p>
+                <p className='details'> <FaLocationDot className='event-icon' /> {location}</p>
+                <p className='details'> <FaTicketAlt className='event-icon' /> Ksh {price}</p>
             </div>
         </div>
     </div>
