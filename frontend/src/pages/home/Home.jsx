e420c031396ac725c6fab8ee23d736073a348bb1
Allan-njoroge <allandavis254@gmail.com>
2024-05-29 22:19:10 +0300

    Added Events using mock data

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import "./Home.css";
 import Card from "../../components/Card/Card";
 import Footer from "../../components/Footer/Footer";
+import EventsData from '../../mockData/events.json'
+import Image from '../../assets/ME.png'
 
 const Home = () => {
+
+  const events = EventsData
+
   return (
     <>
       {/*--Hero Section*/}
@@ -21,18 +26,18 @@ const Home = () => {
       {/*--Events Card Section--*/}
       <section className="events-section">
         <div className="events-wrapper">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {events.map((item, index) => (
+            <Card 
+              key={index} 
+              title={item.title} 
+              date={item.date}
+              description={item.description} 
+              price={item.price}
+              location={item.location}
+              startTime={item.startTime}
+              endTime={item.endTime}
+            />
+          ))}
         </div>
       </section>
       {/*--Footer Section--*/}
