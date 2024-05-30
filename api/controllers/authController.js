18df6b075f495c426efd8ce506337ee36913524f
Allan-njoroge <allandavis254@gmail.com>
2024-05-30 23:55:46 +0300

    Upated backend routes, models and controllers

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,94 +1,11 @@
-const User = require('../models/Users');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // User registration controller
-exports.register = async (req, res) => {
-    try { 
-        // Extract email and password from body
-        const { name, role, email, password } = req.body;
-
-        // Check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Create new user
-        const newUser = new User({
-            name,
-            email,
-            role,
-            password: hashedPassword
-        });
-
-        // Save the user to the database
-        const savedUser = await newUser.save();
-
-        // Respond with the saved user (excluding the password)
-        res.status(200).json({
-            _id: savedUser._id,
-            name: savedUser.name,
-            role: savedUser.role,
-            email: savedUser.email,
-        });
-
-    } catch(err) {
-        res.status(500).json({ error: err.message });
-    }
-};
+exports.register = async (req, res) => {};
 
 // User login controller
-exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Find the user by email
-        const existingUser = await User.findOne({ email });
-        if (!existingUser) {
-            return res.status(404).json({ message: 'User does not exist' });
-        }
-
-        // Compare the password with the hashed password
-        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-        if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Invalid email or password' });
-        }
-
-        // Create a jwt token
-        const token = jwt.sign(
-            { _id: existingUser._id },
-            process.env.JWT_SECRET,
-            { expiresIn: '10s' }
-        );
-
-        // Successful user login message
-        res.status(200).json({ message: `${req.body.email} successfully logged in`, token });
-
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
+exports.login = async (req, res) => {};
 
 // User logout controller
-exports.logout = async (req, res) => {
-    try {
-        // Clear the token cookie
-        res.clearCookie('token');
-        
-        // Destroy the session
-        req.session.destroy((err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Failed to logout' });
-            }
-            res.status(200).json({ message: 'Logged out successfully' });
-        });
-
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.logout = async (req, res) => {};
--- api/controllers/eventsController.js
+++ api/controllers/eventsController.js
@@ -1,146 +1,20 @@
-const Event = require('../models/Events');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-
 // Get all events
-exports.getAllEvents = async (req, res) => {
-    try {
-        const events = await Event.find();
-        res.status(200).json(events);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.getAllEvents = async (req, res) => {};
 
 // Get events by category
-exports.getEventsByCategory = async (req, res) => {
-    try {
-        const category = req.params.category;
-        const events = await Event.find({ category: category });
-
-        if (!events || events.length === 0) {
-            return res.status(404).json({ message: 'There are no events under this category' });
-        }
-
-        res.status(200).json(events);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.getEventsByCategory = async (req, res) => {};
 
 // Get event by ID
-exports.getEventById = async (req, res) => {
-    try {
-        const id = req.params.id;
-
-        // Check if the provided ID is a valid MongoDB ObjectId
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).json({ message: 'Invalid event ID' });
-        }
-
-        const event = await Event.findById(id);
-
-        if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
-        }
-
-        res.status(200).json(event);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.getEventById = async (req, res) => {};
 
 // Get event by title
-exports.getEventByTitle = async (req, res) => {
-    try {
-        const title = req.params.title;
-        const event = await Event.findOne({ title: title });
-
-        if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
-        }
-
-        res.status(200).json(event);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.getEventByTitle = async (req, res) => {};
 
 // Create new event
-exports.createEvent = async (req, res) => {
-    try {
-        const { title, description, image, price, location, date, startTime, endTime } = req.body;
-
-        const newEvent = new Event({ title, description, image, price, location, date, startTime, endTime });
-
-        const savedEvent = await newEvent.save();
-
-        res.status(200).json(savedEvent);
-    } catch (err) {
-        res.status500.json({ message: err.message });
-    }
-};
+exports.createEvent = async (req, res) => {};
 
 // Update event
-exports.updateEvent = async (req, res) => {
-    try {
-        const eventId = req.params.id;
-
-        // Extract the user ID from the JWT token in the request header
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const userId = decodedToken._id;
-
-        const event = await Event.findById(eventId);
-
-        if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
-        }
-
-        if (event.creator.toString() !== userId) {
-            return res.status(403).json({ message: 'Only the creator can update the event' });
-        }
-
-        event.title = req.body.title || event.title;
-        event.description = req.body.description || event.description;
-        event.image = req.body.image || event.image;
-        event.price = req.body.price || event.price;
-        event.location = req.body.location || event.location;
-        event.date = req.body.date || event.date;
-        event.startTime = req.body.startTime || event.startTime;
-        event.endTime = req.body.endTime || event.endTime;
-
-        const updatedEvent = await event.save();
-
-        res.status(200).json(updatedEvent);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.updateEvent = async (req, res) => {};
 
 // Delete event
-exports.deleteEvent = async (req, res) => {
-    try {
-        const eventId = req.params.id;
-
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const userId = decodedToken._id;
-
-        const event = await Event.findById(eventId);
-
-        if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
-        }
-
-        if (event.creator.toString() !== userId) {
-            return res.status(403).json({ message: 'Only the creator can delete the event' });
-        }
-
-        await event.remove();
-
-        res.status(200).json({ message: 'Event deleted successfully' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+exports.deleteEvent = async (req, res) => {};
