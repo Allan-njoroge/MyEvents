bb6e766950e7115b3ed3619768d3491db44489e8
Allan-njoroge <allandavis254@gmail.com>
2024-06-03 20:56:11 +0300

    Endpoints to get events

--- api/routes/events.js
+++ api/routes/events.js
@@ -1,14 +1,13 @@
 import express from 'express'
-import { getAllEvents, getEventsByCategory, getEventById, getEventByTitle, createEvent, updateEvent, deleteEvent, myEvents } from '../controllers/eventsController.js';
+import { getAllEvents, getEventByTitle, deleteEvent, myEvents, addEvent } from '../controllers/eventsController.js';
 
 const router = express.Router()
 
 // Routes to handle events
 router.get('/', getAllEvents);
-router.get('/:id', getEventById);
 router.get('/:title', getEventByTitle);
-router.post('/create-event', createEvent);
 router.delete('/delete/:id', deleteEvent);
 router.get('/my-events', myEvents);
+router.get('/add-event', addEvent);
 
 export default router;
