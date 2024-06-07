77e4db5d4263e42baed71eb599ba4c4689b96fa9
Allan-njoroge <allandavis254@gmail.com>
2024-06-07 21:53:06 +0300

    Login functionality added

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,18 +1,46 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css"
+import axios from 'axios'
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
+
+  const [inputs, setInputs] = useState({
+    emailAddress: "",
+    password: ""
+  })
+  const [error, setError] = useState(null)
+  const navigate = useNavigate()
+
+  const { login } = useContext(AuthContext)
+
+  const handleChange = (e) => {
+    setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await login(inputs)
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+      // setError(err.response.data);
+    }
+  }
+
   return (
     <section className="section">
       <div className="section-container">
         <form action="">
           <h1>LOGIN TO YOUR ACCOUNT</h1>
           <div className="form-div">
-            <input type="email" class="email" placeholder="Email Address" required />
-            <input type="password" class="password" placeholder="Password" required />
+            <input type="email" className="email" placeholder="Email Address" name="emailAddress" required onChange={handleChange} />
+            <input type="password" className="password" placeholder="Password" name="password" required onChange={handleChange} />
 
-            <button type="submit" className="pry-btn">LOGIN</button>
-            <p className="auth-msg"></p>
+            <button type="submit" className="pry-btn" onClick={handleSubmit}>LOGIN</button>
+            { error && <p className="auth-msg">{error}</p> }
           </div>
           <p>DON'T HAVE AN ACCOUNT? <Link to={'/register'}>REGISTER</Link></p>
         </form>
